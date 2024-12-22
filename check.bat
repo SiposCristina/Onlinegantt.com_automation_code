906361773fa06b81b5a44e17d0dc487dc02bf910
Cristina Sipos <157922599+SiposCristina@users.noreply.github.com>
2024-12-22 15:31:37 +0200

    Add files via upload

---
+++ check.bat
@@ -0,0 +1,2 @@
+comp /M Masterfiles\before\Export_to_Excel_File.csv downloads-%1\before\Export_to_Excel_File.csv
+comp /M Masterfiles\after\Export_to_Excel_File.csv downloads-%1\after\Export_to_Excel_File.csv
