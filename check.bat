9169c2b901cbc6f21eae6d4c629399e7ee8ec6fa
Cristina Sipos <157922599+SiposCristina@users.noreply.github.com>
2024-12-28 10:55:42 +0200

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,2 +1,6 @@
 comp /M Masterfiles\before\Export_to_Excel_File.csv downloads-%1\before\Export_to_Excel_File.csv
+comp /M Masterfiles\before\Export_to_Image_File.png downloads-%1\before\Export_to_Image_File.png
+comp /M Masterfiles\before\Export_to_PDF_File.pdf downloads-%1\before\Export_to_PDF_File.pdf
 comp /M Masterfiles\after\Export_to_Excel_File.csv downloads-%1\after\Export_to_Excel_File.csv
+comp /M Masterfiles\after\Export_to_Image_File.png downloads-%1\after\Export_to_Image_File.png
+comp /M Masterfiles\after\Export_to_PDF_File.pdf downloads-%1\after\Export_to_PDF_File.pdf
